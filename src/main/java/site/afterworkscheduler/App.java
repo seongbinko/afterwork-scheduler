969a31e6c9fc27e18f6c1116c3952672671b637b
27조 <79817453+unkwn22@users.noreply.github.com>
2021-05-10 20:42:47 +0900

    chore: app importation change

--- src/main/java/site/afterworkscheduler/App.java
+++ src/main/java/site/afterworkscheduler/App.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import site.afterworkscheduler.scheduler.AfterworkScheduler;
 
 @SpringBootApplication
 @EnableJpaAuditing
