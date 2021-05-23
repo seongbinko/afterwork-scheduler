da487e64152b8ead93110b6a0f6531207745bf22
NamSeok_DT <skatjr30@naver.com>
2021-05-24 03:15:44 +0900

    feat : multi thread

--- src/main/java/site/afterworkscheduler/scheduler/AfterworkScheduler.java
+++ src/main/java/site/afterworkscheduler/scheduler/AfterworkScheduler.java
@@ -49,7 +49,7 @@ public class AfterworkScheduler {
     public static final int DEFAULT_THREADS = 4;
     public static ExecutorService executorService = null;
 
-    @Scheduled(cron = "0 15 3 * * *")
+    @Scheduled(cron = "0 17 3 * * *")
     public void task() throws InterruptedException {
         try {
             System.setProperty(WEB_DRIVER_ID, WEB_DRIVER_PATH);
