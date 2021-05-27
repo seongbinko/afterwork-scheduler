6d4f44fa80a92602337f942c6ef6ff245a17b9b9
NamSeok_DT <skatjr30@naver.com>
2021-05-27 18:40:02 +0900

    chore : Class101 사이트 UI 변경으로 인한 크롤링 수정

--- src/main/java/site/afterworkscheduler/scheduler/AfterworkScheduler.java
+++ src/main/java/site/afterworkscheduler/scheduler/AfterworkScheduler.java
@@ -34,7 +34,7 @@ public class AfterworkScheduler {
     EntityManager em;
 
     // KNS, KSB, CJS 만 변경 시 위에 이넘값으로 변경
-    static ChromeDriverPath chromeDriverPath = ChromeDriverPath.CJS;
+    static ChromeDriverPath chromeDriverPath = ChromeDriverPath.KNS;
 
     public static final String WEB_DRIVER_ID = "webdriver.chrome.driver"; // 드라이버 ID
     public static final String WEB_DRIVER_PATH = chromeDriverPath.getPath(); // 드라이버 경로
@@ -46,7 +46,7 @@ public class AfterworkScheduler {
     public static final int DEFAULT_THREADS = 8;
     public static ExecutorService executorService = null;
 
-    @Scheduled(cron = "0 47 14 * * *")
+    @Scheduled(cron = "0 0 4 * * *")
     public void task() throws InterruptedException {
         try {
             System.setProperty(WEB_DRIVER_ID, WEB_DRIVER_PATH);
@@ -308,6 +308,8 @@ public class AfterworkScheduler {
                         boolean isOnline = true;
                         boolean isOffline = false;
 
+                        webElement.findElement(By.className("gdARiY")).getText();
+
                         strTitle = webElement.findElement(By.className("sp-product-item-thumb-origin")).findElement(By.tagName("img")).getAttribute("alt");
 
                         // 가격이 없는 경우 예외처리
@@ -418,7 +420,6 @@ public class AfterworkScheduler {
     @Transactional
     public Future crawlClass101(ChromeOptions options) {
 
-//        executorService = Executors.newFixedThreadPool(DEFAULT_THREADS);
         executorService = Executors.newCachedThreadPool();
         Runnable runnable = () -> {
             String siteName = "클래스101";
@@ -437,147 +438,164 @@ public class AfterworkScheduler {
                 //webDriver를 해당 url로 이동한다.
                 driver.get(url);
 
-                //브라우저 이동시 생기는 로드시간을 기다린다.
-                //HTTP 응답속도보다 자바의 컴파일 속도가 더 빠르기 때문에 임의적으로 1초를 대기한다.
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-
-                // 무한 스크롤
-                InfiniteScrollForClass101(driver);
-
-                List<WebElement> elList = driver.findElements(By.xpath("//*[@id=\"wrapper\"]/div[1]/main/div/div/div[4]/div[1]/ul/li"));
-                elList.addAll(driver.findElements(By.xpath("//*[@id=\"wrapper\"]/div[1]/main/div/div/div[4]/div[3]/div/div[1]/ul/li")));
-
+                boolean isLastPage = false;
                 List<Product> updateProducts = new ArrayList<>();
-                for (WebElement webElement : elList) {
-                    String strTitle = null;
-                    String strAuthor = null;
-                    int intPrice = 0;
-                    String strPrice = null;
-                    String strPriceInfo = null;
-                    String strImgUrl = null;
-                    String strSiteUrl = null;
-                    String strSiteName = "클래스101";
-                    String strCategory = null;
-                    String strStatus = "Y";
-                    int intPopularity = 0;
-                    boolean isOnline = true;
-                    boolean isOffline = false;
-
+                while(!isLastPage){
+                    //브라우저 이동시 생기는 로드시간을 기다린다.
+                    //HTTP 응답속도보다 자바의 컴파일 속도가 더 빠르기 때문에 임의적으로 1초를 대기한다.
                     try {
-                        //제목
-                        strTitle = webElement.findElement(By.xpath("a/div/div[2]/div[2]")).getText();
-                    } catch (Exception e) {
+                        Thread.sleep(2000);
+                    } catch (InterruptedException e) {
                         e.printStackTrace();
-                        continue;
                     }
-                    strAuthor = webElement.findElement(By.xpath("a/div/div[2]/div[1]/div")).getText().split("・")[1];
 
-                    String strPopularity = webElement.findElement(By.xpath("a/div/div[2]/div[3]/div/div/div[1]")).getText();
-                    intPopularity = PriceStringToIntForClass101(strPopularity);
+                    // 이미지 로딩을 위한 스크롤 다운
+                    InfiniteScrollForClass101(driver);
 
-                    strSiteUrl = webElement.findElement(By.xpath("a")).getAttribute("href");
+                    List<WebElement> elList = driver.findElements(By.xpath("//*[@id=\"wrapper\"]/div[1]/main/div/section/section/section[2]/div/div/div[3]/div[1]/ul/li"));
+                    elList.addAll(driver.findElements(By.xpath("//*[@id=\"wrapper\"]/div[1]/main/div/section/section/section[2]/div/div/div[3]/div[3]/div/div[1]/ul/li")));
 
-                    try {
-                        //int가격을 위한 사전작업
-                        int length = webElement.findElements(By.xpath("a/div/div")).size();
-                        if (length >= 5){
-                            strPrice = webElement.findElement(By.xpath("a/div/div[" + (length - 1)+ "]")).getText();
-                            strPriceInfo = strPrice;
-                            intPrice = PriceStringToIntForClass101(strPrice.split("원")[0]);
+                    List<WebElement> pageElementList = driver.findElements(By.xpath("//*[@id=\"wrapper\"]/div[1]/main/div/section/section/section[2]/div/div/div[3]/section/div/button"));
+
+                    for (WebElement webElement : elList) {
+                        String strTitle = null;
+                        String strAuthor = null;
+                        int intPrice = 0;
+                        String strPrice = null;
+                        String strPriceInfo = null;
+                        String strImgUrl = null;
+                        String strSiteUrl = null;
+                        String strSiteName = "클래스101";
+                        String strCategory = null;
+                        String strStatus = "Y";
+                        int intPopularity = 0;
+                        boolean isOnline = true;
+                        boolean isOffline = false;
+
+                        try {
+                            //제목
+                            strTitle = webElement.findElement(By.xpath("a/div/div[2]/div[2]")).getText();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            continue;
+                        }
+                        strAuthor = webElement.findElement(By.xpath("a/div/div[2]/div[1]/div")).getText().split("・")[1];
+
+                        String strPopularity = webElement.findElement(By.xpath("a/div/div[2]/div[3]/div/div/div[1]")).getText();
+                        intPopularity = PriceStringToIntForClass101(strPopularity);
+
+                        strSiteUrl = webElement.findElement(By.xpath("a")).getAttribute("href");
+
+                        try {
+                            //int가격을 위한 사전작업
+                            int length = webElement.findElements(By.xpath("a/div/div")).size();
+                            if (length >= 5){
+                                strPrice = webElement.findElement(By.xpath("a/div/div[" + (length - 1)+ "]")).getText();
+                                strPriceInfo = strPrice;
+                                intPrice = PriceStringToIntForClass101(strPrice.split("원")[0]);
+                            }
+
+                            //할인가 적용 가격
+                            if (strPrice.contains("월")) {
+                                strPrice = strPrice.replace(" ", "");
+                                strPrice = strPrice.replace("월", "");
+                                strPrice = strPrice.replace("개", "");
+                                strPrice = strPrice.replace("(", "");
+                                strPrice = strPrice.replace(")", "");
+
+                                strPriceInfo = strPrice.split("원")[0] + "원/월 x " + strPrice.split("원")[1];
+                            }
+
+                        } catch (Exception e) {
+                            e.printStackTrace();
                         }
 
-                        //할인가 적용 가격
-                        if (strPrice.contains("월")) {
-                            strPrice = strPrice.replace(" ", "");
-                            strPrice = strPrice.replace("월", "");
-                            strPrice = strPrice.replace("개", "");
-                            strPrice = strPrice.replace("(", "");
-                            strPrice = strPrice.replace(")", "");
-
-                            strPriceInfo = strPrice.split("원")[0] + "원/월 x " + strPrice.split("원")[1];
+                        try {
+                            //이미지 경로
+                            strImgUrl = webElement.findElement(By.tagName("picture")).findElement(By.tagName("img")).getAttribute("src");
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            continue;
                         }
 
-                        System.out.println("strPriceInfo = " + strPriceInfo);
+                        //카테고리 변환
+                        if (krCategory.contains("운동")) {
+                            strCategory = "운동/건강";
+                        } else if (krCategory.contains("직무교육")
+                                || krCategory.contains("개발")
+                                || krCategory.contains("수익")
+                                || krCategory.contains("언어")) {
+                            strCategory = "교육";
+                        } else if (krCategory.contains("공예")
+                                || krCategory.contains("글쓰기")
+                                || krCategory.contains("드로잉")) {
+                            strCategory = "공예";
+                        } else if (krCategory.contains("라이프")
+                                || krCategory.contains("디자인")
+                                || krCategory.contains("사진")) {
+                            strCategory = "아트";
+                        } else if (krCategory.contains("음악")) {
+                            strCategory = "음악";
+                        } else if (krCategory.contains("요리")
+                                || krCategory.contains("베이킹")) {
+                            strCategory = "요리";
+                        } else {
+                            log.info("No Category");
+                        }
 
-                    } catch (Exception e) {
-                        e.printStackTrace();
+                        log.info("strTitle = " + strTitle);
+                        log.info("strAuthor = " + strAuthor);
+                        log.info("intPopularity = " + intPopularity);
+                        log.info("intPrice = " + intPrice);
+                        log.info("strPriceInfo = " + strPriceInfo);
+                        log.info("strImgUrl = " + strImgUrl);
+                        log.info("isOnline = " + isOnline);
+                        log.info("isOffline = " + isOffline);
+                        log.info("strSiteUrl = " + strSiteUrl);
+                        log.info("strSiteName = " + strSiteName);
+                        log.info("strStatus = " + strStatus);
+                        log.info("strCategory = " + strCategory);
+
+                        Category category = categoryRepository.findByName(strCategory).orElse(null);
+
+                        Product product = productRepository.findByTitleLikeAndCategory(strTitle, category).orElse(null);
+                        log.info("&&&&&&&&&&&&&&&&&&&&&&&& 클래스101 &&&&&&&&&&&&&&&&&&&&&&&&");
+                        if (product == null) {
+                            product = Product.builder()
+                                    .title(strTitle)
+                                    .author(strAuthor)
+                                    .popularity(intPopularity)
+                                    .price(intPrice)
+                                    .priceInfo(strPriceInfo)
+                                    .imgUrl(strImgUrl)
+                                    .isOnline(isOnline)
+                                    .isOffline(isOffline)
+                                    .siteUrl(strSiteUrl)
+                                    .siteName(strSiteName)
+                                    .status(strStatus)
+                                    .category(category)
+                                    .build();
+
+                            productRepository.save(product);
+                        } else {
+                            product.setPopularity(intPopularity);
+                            product.setPrice(intPrice);
+                            product.setPriceInfo(strPriceInfo);
+                            product.setImgUrl(strImgUrl);
+                            product.setStatus(strStatus);
+                            product.setSiteName(strSiteName);
+
+                            updateProducts.add(product);
+                        }
                     }
-
-                    try {
-                        //이미지 경로
-                        strImgUrl = webElement.findElement(By.tagName("picture")).findElement(By.tagName("img")).getAttribute("src");
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                        continue;
+                    if (!pageElementList.isEmpty() && pageElementList.get(pageElementList.size() - 1).isEnabled()){
+                        pageElementList.get(pageElementList.size() - 1).sendKeys("\n");
                     }
-
-                    //카테고리 변환
-                    if (krCategory.contains("운동")) {
-                        strCategory = "운동/건강";
-                    } else if (krCategory.contains("커리어")
-                            || krCategory.contains("외국어")
-                            || krCategory.contains("교육")
-                            || krCategory.contains("창업")
-                            || krCategory.contains("비즈니스")
-                            || krCategory.contains("콘텐츠")
-                            || krCategory.contains("자기계발")
-                            || krCategory.contains("온라인쇼핑몰")
-                            || krCategory.contains("부동산")
-                            || krCategory.contains("데이터")) {
-                        strCategory = "교육";
-                    } else if (krCategory.contains("공예")
-                            || krCategory.contains("글쓰기")
-                    ) {
-                        strCategory = "공예";
-                    } else if (krCategory.contains("라이프")
-                            || krCategory.contains("미술")
-                            || krCategory.contains("사진")
-                            || krCategory.contains("디지털")
-                            || krCategory.contains("영상")) {
-                        strCategory = "아트";
-                    } else if (krCategory.contains("음악")) {
-                        strCategory = "음악";
-                    } else if (krCategory.contains("요리")) {
-                        strCategory = "요리";
-                    } else {
-                        log.info("No Category");
-                    }
-                    Category category = categoryRepository.findByName(strCategory).orElse(null);
-
-                    Product product = productRepository.findByTitleLikeAndCategory(strTitle, category).orElse(null);
-                    log.info("&&&&&&&&&&&&&&&&&&&&&&&& 클래스101 &&&&&&&&&&&&&&&&&&&&&&&&");
-                    if (product == null) {
-                        product = Product.builder()
-                                .title(strTitle)
-                                .author(strAuthor)
-                                .popularity(intPopularity)
-                                .price(intPrice)
-                                .priceInfo(strPriceInfo)
-                                .imgUrl(strImgUrl)
-                                .isOnline(isOnline)
-                                .isOffline(isOffline)
-                                .siteUrl(strSiteUrl)
-                                .siteName(strSiteName)
-                                .status(strStatus)
-                                .category(category)
-                                .build();
-
-                        productRepository.save(product);
-                    } else {
-                        product.setPopularity(intPopularity);
-                        product.setPrice(intPrice);
-                        product.setPriceInfo(strPriceInfo);
-                        product.setImgUrl(strImgUrl);
-                        product.setStatus(strStatus);
-                        product.setSiteName(strSiteName);
-
-                        updateProducts.add(product);
+                    else{
+                        isLastPage = true;
                     }
                 }
+
                 productRepository.saveAll(updateProducts);
                 log.info("총 update하는 product size: " + updateProducts.size());
             }
@@ -847,7 +865,7 @@ public class AfterworkScheduler {
             //브라우저 이동시 생기는 로드시간을 기다린다.
             //HTTP 응답속도보다 자바의 컴파일 속도가 더 빠르기 때문에 임의적으로 0.1초를 대기한다.
             try {
-                Thread.sleep(100);
+                Thread.sleep(1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -1682,7 +1700,7 @@ public class AfterworkScheduler {
             jse.executeScript("window.scrollTo({top:document.body.scrollHeight, left:0, behavior:'smooth'})");
 
             try {
-                Thread.sleep(3000);
+                Thread.sleep(1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -1732,3 +1750,4 @@ public class AfterworkScheduler {
         return Integer.valueOf(price);
     }
 }
+
